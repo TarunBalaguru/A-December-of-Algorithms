0a2c92541f6e36933b0e1cf785b9330a291b3b96
TarunBalaguru <45656838+TarunBalaguru@users.noreply.github.com>
2018-12-06 16:00:04 +0530

    Binary Search

--- December-01/python_exampleUser.py
+++ December-01/python_exampleUser.py
@@ -1,8 +1,22 @@
-#!python3
-
-"""
-Hi there.
-This file doesn't contain any code.
-It's just here to give an example of the file naming scheme.
-Cheers!
-"""
+#include<stdio.h>
+void main()
+  {
+    int x,y;
+    clrsccr();
+    printf("Enter a number:");
+    scanf("%d",&x);
+    clrscr();
+    do
+      {
+        printf("Guess the number");
+        scanf("%d",&y);
+        if(x>y)
+          printf("Too Low !!!\n);
+        else if(x<y)
+          printf("Too High !!!\n);
+        else
+          printf("Spot on");
+       }while(x!=y);
+   }
+        
+        
